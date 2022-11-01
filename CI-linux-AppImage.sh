3d565ef20313ad1ed0ccb670341dc7c740811a69
zaraka <zarakaangel@gmail.com>
2022-11-02 00:18:59 +0100

    preparation to cmake appimage option

--- CI-linux-AppImage.sh
+++ CI-linux-AppImage.sh
@@ -2,8 +2,6 @@
 
 set -o verbose
 
-PATH=/home/zaraka/Qt/5.15.2/gcc_64/bin/:$PATH
-
 # Check versions
 qmake -v
 cmake --version
@@ -15,14 +13,12 @@ mkdir buildAppImage
 cd buildAppImage
 cmake .. -DCMAKE_PREFIX_PATH="cmake/packages" -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Werror" -DCMAKE_EXE_LINKER_FLAGS="-Wl,--fatal-warnings" -DTB_SUPPRESS_PCH=1 || exit 1
 #cmake --build . --config Release || exit 1
-make -j 24
+make -j
 make install DESTDIR=AppDir
 
-# not needed QMAKE=$$QMAKE_QMAKE QML_SOURCES_PATHS=$$QML_IMPORT_PATH
-# need to be resolved VERSION=$${VERSION_MAJOR}.$${VERSION_MINOR}-$${VERSION_BUILD}
-# not needed LD_LIBRARY_PATH=$$LD_LIBRARY_PATH:$${PWD}/Dependencies/NDI/lib:/usr/lib64/samba/
+# TODO: need to be resolved VERSION=??
 
-# resolve how to deploy desktop file and icons
+# TODO:
 mkdir -p AppDir/usr/share/applications/
 cp ../app/resources/linux/trenchbroom.desktop AppDir/usr/share/applications/
 mkdir -p AppDir/usr/share/hicolor/256x256/apps/
