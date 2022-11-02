2c34d771fefdfe10c64e7f8038bea8f4288d2d7a
zaraka <zarakaangel@gmail.com>
2022-11-02 11:10:25 +0100

    add APPIMAGE_BUILD option

--- CI-linux-AppImage.sh
+++ CI-linux-AppImage.sh
@@ -11,17 +11,11 @@ pandoc --version
 
 mkdir buildAppImage
 cd buildAppImage
-cmake .. -DCMAKE_PREFIX_PATH="cmake/packages" -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Werror" -DCMAKE_EXE_LINKER_FLAGS="-Wl,--fatal-warnings" -DTB_SUPPRESS_PCH=1 || exit 1
-#cmake --build . --config Release || exit 1
+cmake .. -DCMAKE_PREFIX_PATH="cmake/packages" -DAPPIMAGE_BUILD=ON -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Werror" -DCMAKE_EXE_LINKER_FLAGS="-Wl,--fatal-warnings" -DTB_SUPPRESS_PCH=1 || exit 1
 make -j
 make install DESTDIR=AppDir
 
 # TODO: need to be resolved VERSION=??
-
-# TODO:
-mkdir -p AppDir/usr/share/applications/
-cp ../app/resources/linux/trenchbroom.desktop AppDir/usr/share/applications/
-mkdir -p AppDir/usr/share/hicolor/256x256/apps/
-cp ../app/resources/linux/icons/icon_256.png AppDir/usr/share/hicolor/256x256/apps/trenchbroom.png
-
-linuxdeploy-x86_64.AppImage --appdir AppDir -e "AppDir/usr/bin/trenchbroom" -i "AppDir/usr/share/hicolor/256x256/apps/trenchbroom.png" -d "AppDir/usr/share/applications/trenchbroom.desktop" --plugin qt --output appimage
+linuxdeploy-x86_64.AppImage --appdir AppDir -e "AppDir/usr/bin/trenchbroom" \
+                                            -d "AppDir/usr/share/applications/trenchbroom.desktop" \
+                                            --plugin qt --output appimage
